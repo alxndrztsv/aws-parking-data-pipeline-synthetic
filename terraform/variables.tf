e47ac65f3006d52a119b00f706f4201992afe8d6
alxndrztsv <alxndrzaitsev@gmail.com>
2026-08-25 22:55:23 +0100

    Minor fixes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,8 +6,8 @@ variable "aws_region" {
 }
 
 variable "environment" {
-	  description = "Environment (e.g., dev, staging, prod)."
-	  type        = string
+	description = "Environment (e.g., dev, staging, prod)."
+	type        = string
 }
 
 variable "project_name" {
@@ -16,21 +16,21 @@ variable "project_name" {
 }
 
 variable "sender_email" {
-		description = "Email address to send reports from (must be verified in SES)."
-	  type    = string
+	description = "Email address to send reports from (must be verified in SES)."
+	type    	= string
 }
 
 variable "recipient_email" {
-		description = "Email address to receive reports."
-	  type = string
+	description = "Email address to receive reports."
+	type 		= string
 }
 
 variable "failure_notification_email" {
-	  description = "Email address for pipeline alerts"
-	  type        = string
+	description = "Email address for pipeline alerts"
+	type        = string
 	  
-	  validation {
-		    condition     = can(regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", var.failure_notification_email))
-		    error_message = "Must be a valid email address."
-	  }
+	validation {
+		condition     = can(regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", var.failure_notification_email))
+		error_message = "Must be a valid email address."
+	}
 }
